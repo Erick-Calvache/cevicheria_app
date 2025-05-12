52af6423c53e9fdd1bb503823d63cde75b39b942
Erick-Calvache <lzeroxtwo@gmail.com>
2025-05-12 15:21:39 -0500

    Organizacion de carpetas y archivos

--- lib/pages/productos_page.dart
+++ lib/pages/productos_page.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-import 'theme.dart';
+import '../theme.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
